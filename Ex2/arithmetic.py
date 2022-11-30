20ddbbedd08f8dc68002d349394993de90179ab8
Admin <Admin@FL-66.ffdc.audist>
2022-11-30 15:48:19 +0530

    EX2

---
+++ Ex2/arithmetic.py
@@ -0,0 +1,10 @@
+x=int(input("Enter first number "))
+y=int(input("Enter second number "))
+
+print("Sum is : ",x+y)
+print("Difference is : ",x-y)
+print("Product is : ",x*y)
+print("Quotient is : ",x/y)#this gives quotient in points also 
+print("Floor division is : ",x//y)#this results quotient as number before points 
+print("Remainder is : ",x%y)
+print("Exponential is : ",x**y)
---
+++ Ex2/stringConcat.py
@@ -0,0 +1,13 @@
+x=input("Enter first string : ")
+y=input("Enter second String : ")
+
+#method1
+print("String Cocatenation using + operator ",x+y)
+#method2
+print("String Concatenation Using join()" , " ------------  ".join([x,y]))
+#method3
+print("String Concatenation using place holder operator %s %s"%(x,y))
+#method4
+print("String conxcatenation Using format() ","{}*****************************************{}".format(x,y))
+#method%
+print("String Concatination using f-string ", f'{x}##############################################{y}')
